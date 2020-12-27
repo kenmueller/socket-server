07579442963b77e80a7938a01d985f125123e133
kenmueller <kenmueller0@gmail.com>
2020-12-27 15:11:46 -0800

    typealias

--- Sources/Socket/SocketClient.swift
+++ Sources/Socket/SocketClient.swift
@@ -1,6 +1,8 @@
 public protocol SocketClient {
+	typealias Room = SocketRoom<Self>
+	
 	associatedtype Share: SocketShare = SocketDefaultShare
 	associatedtype Query: SocketQuery = SocketDefaultQuery
 	
-	init(room: SocketRoom<Self>, socket: Socket, query: Query)
+	init(room: Room, socket: Socket, query: Query)
 }
